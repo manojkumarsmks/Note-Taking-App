e64b26e03ff60743443de145524dc94b0deec031
manojkumarsmks <manojkumarsmks@gmail.com>
2018-04-30 14:49:41 -0600

    Basic list view

--- app/src/main/java/com/cs193a/manojkumar/notetakingapp/MainActivity.java
+++ app/src/main/java/com/cs193a/manojkumar/notetakingapp/MainActivity.java
@@ -2,15 +2,33 @@ package com.cs193a.manojkumar.notetakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.simple_list_view);
+
+        // Load the data from the list_view_content to a string array
+        String[] marvelMovies = getResources().getStringArray(R.array.list_view_contect);
+
+        // Setup an adapter
+        ArrayAdapter adapter = new ArrayAdapter(
+                // Context
+                this,
+                // Simple list of Items
+                android.R.layout.simple_list_item_1,
+                // Source for the list
+                marvelMovies);
+
+        listView.setAdapter(adapter);
+
     }
 }
